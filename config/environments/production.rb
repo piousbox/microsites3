ffc10ccead244c53adc1be11c57a2dcd49bac58c
Victor Piousbox <piousbox@gmail.com>
2015-10-08 12:46:00 -0700

    set some secret key base

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,6 +16,7 @@ Microsites2::Application.configure do
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
+  config.secret_key_base = 'some-secret-key-base'
 
   # Generate digests for assets URLs
   config.assets.digest = true
