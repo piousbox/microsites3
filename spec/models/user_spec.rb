995eb89e50873b84bfaa34a1e325a612ed1b7ae2
piousbox <piousbox@gmail.com>
2017-05-26 11:02:13 -0500

    specs pass

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,7 @@
-# require 'rails_helper'
+require 'spec_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'does' do
+    true.should eql true
+  end
 end
