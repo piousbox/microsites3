206bc8f5a78b20930f26393be53aa47fb5ae9f78
piousbox <piousbox@gmail.com>
2017-05-27 10:59:07 -0500

    version bumps, upload photos

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -49,4 +49,16 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
+=begin
+  config.paperclip_defaults = {
+    :storage => :s3,
+    :s3_region => ENV['AWS_REGION'],
+    :s3_credentials => {
+      :bucket => ENV['AWS_BUCKET'],
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    }
+  }
+=end
+
 end
