e49d8adab1d6eb25bee2a25f6d9d8ef12a5b35ac
piousbox <piousbox@gmail.com>
2017-12-05 17:20:03 -0800

    better access denied

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,21 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!, :except => [ :test ]
+  
   def home
   end
 
   def test
     render :json => { :status => :ok }
   end
+
+=begin
+  rescue_from ::CanCan::AccessDenied, :with => :access_denied
+  private
+  def access_denied exception
+    store_location_for :user, request.path
+    redirect_to user_signed_in? ? root_path : new_user_session_path, :alert => exception.message
+  end
+=end
 
 end
