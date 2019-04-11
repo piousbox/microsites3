8cdfdf6d907a35527c63703f4602891d0d9abe6d
Victor Pudeyev <piousbox@gmail.com>
2019-04-11 00:46:27 -0500

    which versions of my libs?

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,6 +1,7 @@
 lock "3.8.1"
 
 set :application, "microsites3"
+set :ruby_version, "2.5.0"
 set :repo_url, "git@github.com:piousbox/microsites3.git"
 
 append :linked_files, "config/initializers/00_s3.rb", "config/mongoid.yml", "config/initializers/koala.rb", "config/initializers/05_stripe.rb"
@@ -10,7 +11,7 @@ set :deploy_via, :remote_cache
 namespace :deploy do
   task :bundle do
     on roles(:web) do
-      execute "cd /home/#{fetch(:app_user)}/projects/microsites3/current && sudo /home/#{fetch(:app_user)}/.rbenv/versions/2.3.1/bin/bundle --path /home/#{fetch(:app_user)}/projects/microsites3/vendor/bundle"
+      execute "cd /home/#{fetch(:app_user)}/projects/microsites3/current && sudo /home/#{fetch(:app_user)}/.rbenv/versions/#{fetch(:ruby_version)}/bin/bundle --path /home/#{fetch(:app_user)}/projects/microsites3/vendor/bundle"
     end
   end
 
