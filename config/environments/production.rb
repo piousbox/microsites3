76470dda1eb71c0d2797f0315d080daa858dcb49
Victor Pudeyev <piousbox@gmail.com>
2020-02-07 14:42:00 -0600

    no more errors for everybody

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,7 @@
 Rails.application.configure do
   config.cache_classes = true
   config.eager_load = true
-  config.consider_all_requests_local = true
+  config.consider_all_requests_local = false
   config.action_controller.perform_caching = true
   config.read_encrypted_secrets = true
   config.public_file_server.enabled = true # ENV['RAILS_SERVE_STATIC_FILES'].present?
